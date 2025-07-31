ca64777ebd5794a2c77aaf604195663f0ca4cece
joeylynchhh <joseph.lynch.dr@gmail.com>
2025-07-31 09:21:42 -0700

    Basic template built for a dashboard application.

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -16,16 +16,10 @@ const iconMap = {
 export default async function CardWrapper() {
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
+      <Card title="Collected" value={100} type="collected" />
+      <Card title="Pending" value={100} type="pending" />
+      <Card title="Total Invoices" value={100} type="invoices" />
+      <Card title="Total Customers" value={100} type="customers" />
     </>
   );
 }
